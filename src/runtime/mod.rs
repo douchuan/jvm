e6a04e3b803f21c797f054f4eb84eeb1be98797e
Dou Chuan <1843657913@qq.com>
2019-12-15 19:51:45 +0800

    refact to shorter name

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,18 +1,18 @@
 #![allow(unused)]
 
 mod class_loader;
-mod class_path_manager;
+mod cp_manager;
 mod execution;
 mod frame;
 mod local;
 mod stack;
-mod system_dictionary;
+mod sys_dic;
 
 pub use class_loader::{require_class, require_class2, ClassLoader};
-pub use class_path_manager::{init as cp_path_init, find_class as find_class_in_classpath, ClassPathResult, ClassSource};
+pub use cp_manager::{init as cp_path_init, find_class as find_class_in_classpath, ClassPathResult, ClassSource};
 pub use execution::instance_of;
 pub use frame::Frame;
 pub use local::Local;
 pub use local::Slot;
 pub use stack::Stack;
-pub use system_dictionary::{find as sys_dic_find, init as sys_dic_init, put as sys_dic_put};
+pub use sys_dic::{find as sys_dic_find, init as sys_dic_init, put as sys_dic_put};
