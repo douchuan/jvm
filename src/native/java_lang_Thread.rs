0c129bf09ce8918f78348ed0879cd35a8b152947
Dou Chuan <1843657913@qq.com>
2020-02-09 15:09:54 +0800

    impl java.lang.Thread start0

--- src/native/java_lang_Thread.rs
+++ src/native/java_lang_Thread.rs
@@ -1,9 +1,10 @@
 #![allow(non_snake_case)]
 
 use crate::native::{new_fn, JNIEnv, JNINativeMethod, JNIResult};
-use crate::oop::OopRef;
-use crate::runtime::JavaThread;
+use crate::oop::{self, OopRef, OopDesc};
+use crate::runtime::{JavaThread, JavaCall, Stack};
 use std::sync::{Arc, Mutex};
+use crate::util::new_method_id;
 
 pub fn get_native_methods() -> Vec<JNINativeMethod> {
     vec![
@@ -14,6 +15,8 @@ pub fn get_native_methods() -> Vec<JNINativeMethod> {
             Box::new(jvm_currentThread),
         ),
         new_fn("setPriority0", "(I)V", Box::new(jvm_setPriority0)),
+        new_fn("isAlive", "()Z", Box::new(jvm_isAlive)),
+        new_fn("start0", "()V", Box::new(jvm_start0)),
     ]
 }
 
@@ -29,4 +32,52 @@ fn jvm_currentThread(jt: &mut JavaThread, env: JNIEnv, args: Vec<OopRef>) -> JNI
 fn jvm_setPriority0(jt: &mut JavaThread, env: JNIEnv, args: Vec<OopRef>) -> JNIResult {
     //todo: set native thread's priority
     Ok(None)
+}
+
+fn jvm_isAlive(jt: &mut JavaThread, env: JNIEnv, args: Vec<OopRef>) -> JNIResult {
+    //todo: impl
+    Ok(Some(OopDesc::new_int(0)))
+}
+
+fn jvm_start0(jt: &mut JavaThread, env: JNIEnv, args: Vec<OopRef>) -> JNIResult {
+    let thread_oop = {
+        match args.get(0) {
+            Some(v) => v.clone(),
+            _ => unreachable!()
+        }
+    };
+
+    let cls = {
+        let v= thread_oop.lock().unwrap();
+        match &v.v {
+            oop::Oop::Inst(inst) => inst.class.clone(),
+            _ => unreachable!()
+        }
+    };
+
+    let name = {
+        let cls = cls.lock().unwrap();
+        cls.name.clone()
+    };
+
+    if String::from_utf8_lossy(name.as_slice()) == "java/lang/ref/Reference$ReferenceHandler" {
+        Ok(None)
+    } else {
+
+        //todo: impl threads manager
+
+        let mir = {
+            let cls = cls.lock().unwrap();
+            let id = new_method_id(b"run", b"()V");
+            cls.get_virtual_method(id).unwrap()
+        };
+
+        let mut jt = JavaThread::new();
+        let mut stack = Stack::new(0);
+        let args = vec![thread_oop];
+        let mut jc = JavaCall::new_with_args(&mut jt, mir, args);
+        jc.invoke(&mut jt, &mut stack, false);
+
+        Ok(None)
+    }
 }
