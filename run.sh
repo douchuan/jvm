4355a0e5c74ace685374ba77f2882f2cf806b821
Dou Chuan <1843657913@qq.com>
2020-02-13 21:40:09 +0800

    convenient script

---
+++ run.sh
@@ -0,0 +1,3 @@
+RUST_LOG=trace RUST_BACKTRACE=1 cargo run -- --cp .:test/zulu8/jre/lib/rt.jar:test/zulu8/jre/lib/charsets.jar test/HelloWorld
+#RUST_LOG=trace RUST_BACKTRACE=1 cargo run -- --cp .:test/zulu8/jre/lib/rt.jar test/HelloWorld
+#RUST_LOG=trace RUST_BACKTRACE=1 cargo run -- --cp .:test/zulu8/jre/lib/rt.jar test/Add
