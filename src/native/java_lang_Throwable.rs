54c3319d1806dd4e5402e488920039194b8d6fce
Dou Chuan <1843657913@qq.com>
2020-02-16 10:36:20 +0800

    从Method中构造StackTraceElement
    避免获取Frame中的信息，因为frame此时是被JavaCalllock住的

--- src/native/java_lang_Throwable.rs
+++ src/native/java_lang_Throwable.rs
@@ -19,57 +19,39 @@ fn jvm_fillInStackTrace(jt: &mut JavaThread, _env: JNIEnv, args: Vec<OopRef>) ->
     let ary_cls = require_class3(None, b"[Ljava/lang/StackTraceElement;").unwrap();
 
     let throwable_oop = args.get(0).unwrap();
-    let mut elms = Vec::new();
-    let frames = jt.frames.clone();
+    let callers = jt.callers.clone();
 
-    for it in frames.iter() {
-        match it.try_lock() {
-            Ok(frame) => {
-                let mir = frame.mir.clone();
-                let pc = frame.pc;
-
-                //todo: native frame
-                let line_number = {
-                    let pc = pc as u16;
-                    match mir.method.lnt.get(&pc) {
-                        Some(&v) => v as i32,
-                        _ => -2,
-                    }
-                };
-
-                let cls_name = { mir.method.class.lock().unwrap().name.clone() };
-                let method_name = mir.method.name.clone();
-                let src_file = mir.method.src_file.clone();
-                let src_file = match src_file {
-                    Some(name) => {
-                        //                        println!("src_file = {}", String::from_utf8_lossy(name.as_slice()));
-                        util::oop::new_java_lang_string3(jt, name.as_slice())
-                    }
-                    None => util::oop::new_java_lang_string2(jt, ""),
-                };
-
-                let elm = OopDesc::new_inst(elm_cls.clone());
-                let args = vec![
-                    elm.clone(),
-                    util::oop::new_java_lang_string3(jt, cls_name.as_slice()),
-                    util::oop::new_java_lang_string3(jt, method_name.as_slice()),
-                    src_file,
-                    OopDesc::new_int(line_number),
-                ];
-                runtime::java_call::invoke_ctor(
-                    jt,
-                    elm_cls.clone(),
-                    b"(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;I)V",
-                    args,
-                );
-
-                elms.push(elm);
+    let mut traces= Vec::new();
+    for mir in callers.iter().rev() {
+        let cls_name = { mir.method.class.lock().unwrap().name.clone() };
+        let method_name = mir.method.name.clone();
+        let src_file = mir.method.src_file.clone();
+        let src_file = match src_file {
+            Some(name) => {
+                util::oop::new_java_lang_string3(jt, name.as_slice())
             }
-            _ => break,
-        }
+            None => util::oop::new_java_lang_string2(jt, ""),
+        };
+
+        let elm = OopDesc::new_inst(elm_cls.clone());
+        let args = vec![
+            elm.clone(),
+            util::oop::new_java_lang_string3(jt, cls_name.as_slice()),
+            util::oop::new_java_lang_string3(jt, method_name.as_slice()),
+            src_file,
+            OopDesc::new_int(0),
+        ];
+        runtime::java_call::invoke_ctor(
+            jt,
+            elm_cls.clone(),
+            b"(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;I)V",
+            args,
+        );
+
+        traces.push(elm);
     }
 
-    let stack_trace_ary = OopDesc::new_ref_ary2(ary_cls, elms);
+    let stack_trace_ary = OopDesc::new_ref_ary2(ary_cls, traces);
     let throwable_cls = require_class3(None, b"java/lang/Throwable").unwrap();
     {
         let cls = throwable_cls.lock().unwrap();
