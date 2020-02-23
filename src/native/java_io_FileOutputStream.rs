669b37a32ea6fcd0b15e0ce4796f94cba68055d1
Dou Chuan <1843657913@qq.com>
2020-02-23 14:50:03 +0800

    把writeBytes实现完整

--- src/native/java_io_FileOutputStream.rs
+++ src/native/java_io_FileOutputStream.rs
@@ -2,8 +2,9 @@
 
 use crate::native::{new_fn, JNIEnv, JNINativeMethod, JNIResult};
 use crate::oop::{self, Oop};
-use crate::runtime::JavaThread;
+use crate::runtime::{require_class3, JavaThread};
 use crate::types::OopRef;
+use crate::util;
 
 pub fn get_native_methods() -> Vec<JNINativeMethod> {
     vec![
@@ -17,52 +18,56 @@ fn jvm_initIDs(_jt: &mut JavaThread, _env: JNIEnv, _args: Vec<OopRef>) -> JNIRes
 }
 
 fn jvm_writeBytes(_jt: &mut JavaThread, _env: JNIEnv, args: Vec<OopRef>) -> JNIResult {
-    let _os = args.get(0).unwrap();
+    let os = args.get(0).unwrap();
+    let fd = get_file_descriptor_fd(os.clone());
     let byte_ary = args.get(1).unwrap();
     let off = {
         let v = args.get(2).unwrap();
-        let v = v.lock().unwrap();
-        match v.v {
-            Oop::Int(v) => v,
-            _ => unreachable!(),
-        }
+        util::oop::extract_int(v.clone())
     };
     let len = {
         let v = args.get(3).unwrap();
-        let v = v.lock().unwrap();
-        match v.v {
-            Oop::Int(v) => v,
-            _ => unreachable!(),
-        }
+        util::oop::extract_int(v.clone())
     };
-    let _append = {
+    let is_append = {
         let v = args.get(4).unwrap();
-        let v = v.lock().unwrap();
-        match v.v {
-            Oop::Int(v) => v,
-            _ => unreachable!(),
-        }
+        util::oop::extract_int(v.clone()) == 1
     };
 
-    let bytes: Vec<u8> = {
-        let v = byte_ary.lock().unwrap();
-        match &v.v {
-            Oop::TypeArray(ary) => match ary {
-                oop::TypeArrayValue::Byte(ary) => ary[off as usize..(off + len) as usize]
-                    .iter()
-                    .map(|v| *v)
-                    .collect(),
-                t => unreachable!("t = {:?}", t),
-            },
-            _ => unreachable!(),
-        }
-    };
+    let v = byte_ary.lock().unwrap();
+    match &v.v {
+        Oop::TypeArray(ary) => match ary {
+            oop::TypeArrayValue::Byte(ary) => {
+                let (_, ary) = ary.split_at(off as usize);
+                let len = len as usize;
+                let ary = &ary[..len];
 
-    //    println!("xxxxxx 1");
-    print!("{}", String::from_utf8_lossy(bytes.as_slice()));
-    //    print!("{:?}, len={}", bytes.as_slice(), bytes.len());
-    //    io::stdout().flush().unwrap();
-    //    print!("{}", String::from_utf8_lossy("\n".as_bytes()));
+                unsafe {
+                    libc::write(fd, ary.as_ptr() as *const libc::c_void, len);
+                }
+            }
+            t => unreachable!("t = {:?}", t),
+        },
+        _ => unreachable!(),
+    }
 
     Ok(None)
+}
+
+fn get_file_descriptor_fd(fin: OopRef) -> i32 {
+    let cls = require_class3(None, b"java/io/FileOutputStream").unwrap();
+    let fd_this = {
+        let cls = cls.lock().unwrap();
+        let id = cls.get_field_id(b"fd", b"Ljava/io/FileDescriptor;", false);
+        cls.get_field_value(fin.clone(), id)
+    };
+
+    let cls = require_class3(None, b"java/io/FileDescriptor").unwrap();
+    let fd = {
+        let cls = cls.lock().unwrap();
+        let id = cls.get_field_id(b"fd", b"I", false);
+        cls.get_field_value(fd_this, id)
+    };
+
+    util::oop::extract_int(fd)
 }
