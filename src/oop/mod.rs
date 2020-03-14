e50a73d8cce6ac450d3b1fa782816a11c3192896
Dou Chuan <1843657913@qq.com>
2020-03-14 09:30:27 +0800

    refactor: extract values mod

--- src/oop/mod.rs
+++ src/oop/mod.rs
@@ -10,23 +10,10 @@ pub mod class;
 pub mod consts;
 pub mod field;
 pub mod method;
+pub mod values;
 
 pub use self::class::{Class, ClassKind};
-
-#[derive(Debug, Clone, Copy, PartialOrd, PartialEq)]
-pub enum ValueType {
-    BYTE,
-    BOOLEAN,
-    CHAR,
-    SHORT,
-    INT,
-    LONG,
-    FLOAT,
-    DOUBLE,
-    VOID,
-    OBJECT,
-    ARRAY,
-}
+pub use self::values::ValueType;
 
 #[derive(Debug)]
 pub enum RefKind {
@@ -266,76 +253,6 @@ impl RefKindDesc {
     pub fn monitor_exit(&mut self) {
         let mut v = self.monitor.lock().unwrap();
         *v -= 1;
-    }
-}
-
-impl From<&u8> for ValueType {
-    fn from(v: &u8) -> Self {
-        match v {
-            b'B' => ValueType::BYTE,
-            b'Z' => ValueType::BOOLEAN,
-            b'C' => ValueType::CHAR,
-            b'S' => ValueType::SHORT,
-            b'I' => ValueType::INT,
-            b'J' => ValueType::LONG,
-            b'F' => ValueType::FLOAT,
-            b'D' => ValueType::DOUBLE,
-            b'V' => ValueType::VOID,
-            b'L' => ValueType::OBJECT,
-            b'[' => ValueType::ARRAY,
-            t => {
-                let s = [*t];
-                let s = String::from_utf8_lossy(&s);
-                unreachable!("Unknown ValueType = {}", s)
-            }
-        }
-    }
-}
-
-impl Into<&[u8]> for ValueType {
-    fn into(self) -> &'static [u8] {
-        match self {
-            ValueType::BYTE => b"B",
-            ValueType::BOOLEAN => b"Z",
-            ValueType::CHAR => b"C",
-            ValueType::SHORT => b"S",
-            ValueType::INT => b"I",
-            ValueType::LONG => b"J",
-            ValueType::FLOAT => b"F",
-            ValueType::DOUBLE => b"D",
-            ValueType::VOID => b"V",
-            ValueType::OBJECT => b"L",
-            ValueType::ARRAY => b"[",
-        }
-    }
-}
-
-impl ValueType {
-    pub fn parse_wrap(class_loader: Option<ClassLoader>, desc: &str) -> Self {
-        match desc.as_bytes().first().unwrap() {
-            b'B' | b'Z' | b'C' | b'S' | b'I' => ValueType::INT,
-            b'J' => ValueType::LONG,
-            b'F' => ValueType::FLOAT,
-            b'D' => ValueType::DOUBLE,
-            b'V' => ValueType::VOID,
-            b'L' => ValueType::OBJECT,
-            b'[' => ValueType::ARRAY,
-            _ => unreachable!(),
-        }
-    }
-
-    pub fn into_primitive_name(&self) -> &'static [u8] {
-        match *self {
-            ValueType::BYTE => b"byte",
-            ValueType::BOOLEAN => b"boolean",
-            ValueType::CHAR => b"char",
-            ValueType::SHORT => b"short",
-            ValueType::INT => b"int",
-            ValueType::LONG => b"long",
-            ValueType::FLOAT => b"float",
-            ValueType::DOUBLE => b"double",
-            ValueType::VOID | ValueType::OBJECT | ValueType::ARRAY => unreachable!(),
-        }
     }
 }
 
---
+++ src/oop/values.rs
@@ -0,0 +1,86 @@
+use crate::runtime::ClassLoader;
+
+#[derive(Debug, Clone, Copy, PartialOrd, PartialEq)]
+pub enum ValueType {
+    BYTE,
+    BOOLEAN,
+    CHAR,
+    SHORT,
+    INT,
+    LONG,
+    FLOAT,
+    DOUBLE,
+    VOID,
+    OBJECT,
+    ARRAY,
+}
+
+impl From<&u8> for ValueType {
+    fn from(v: &u8) -> Self {
+        match v {
+            b'B' => ValueType::BYTE,
+            b'Z' => ValueType::BOOLEAN,
+            b'C' => ValueType::CHAR,
+            b'S' => ValueType::SHORT,
+            b'I' => ValueType::INT,
+            b'J' => ValueType::LONG,
+            b'F' => ValueType::FLOAT,
+            b'D' => ValueType::DOUBLE,
+            b'V' => ValueType::VOID,
+            b'L' => ValueType::OBJECT,
+            b'[' => ValueType::ARRAY,
+            t => {
+                let s = [*t];
+                let s = String::from_utf8_lossy(&s);
+                unreachable!("Unknown ValueType = {}", s)
+            }
+        }
+    }
+}
+
+impl Into<&[u8]> for ValueType {
+    fn into(self) -> &'static [u8] {
+        match self {
+            ValueType::BYTE => b"B",
+            ValueType::BOOLEAN => b"Z",
+            ValueType::CHAR => b"C",
+            ValueType::SHORT => b"S",
+            ValueType::INT => b"I",
+            ValueType::LONG => b"J",
+            ValueType::FLOAT => b"F",
+            ValueType::DOUBLE => b"D",
+            ValueType::VOID => b"V",
+            ValueType::OBJECT => b"L",
+            ValueType::ARRAY => b"[",
+        }
+    }
+}
+
+impl ValueType {
+    pub fn parse_wrap(class_loader: Option<ClassLoader>, desc: &str) -> Self {
+        match desc.as_bytes().first().unwrap() {
+            b'B' | b'Z' | b'C' | b'S' | b'I' => ValueType::INT,
+            b'J' => ValueType::LONG,
+            b'F' => ValueType::FLOAT,
+            b'D' => ValueType::DOUBLE,
+            b'V' => ValueType::VOID,
+            b'L' => ValueType::OBJECT,
+            b'[' => ValueType::ARRAY,
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn into_primitive_name(&self) -> &'static [u8] {
+        match *self {
+            ValueType::BYTE => b"byte",
+            ValueType::BOOLEAN => b"boolean",
+            ValueType::CHAR => b"char",
+            ValueType::SHORT => b"short",
+            ValueType::INT => b"int",
+            ValueType::LONG => b"long",
+            ValueType::FLOAT => b"float",
+            ValueType::DOUBLE => b"double",
+            ValueType::VOID | ValueType::OBJECT | ValueType::ARRAY => unreachable!(),
+        }
+    }
+}
