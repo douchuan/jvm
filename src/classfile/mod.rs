127a63083361eb20a4f15d935cb413de36c08c79
Dou Chuan <1843657913@qq.com>
2020-03-15 09:58:14 +0800

    refactor: extract classfile & version mod

--- src/classfile/mod.rs
+++ src/classfile/mod.rs
@@ -2,6 +2,7 @@
 
 pub mod attributes;
 pub mod checker;
+pub mod classfile;
 pub mod constant_pool;
 pub mod consts;
 pub mod field_info;
@@ -9,47 +10,12 @@ pub mod flags;
 pub mod method_info;
 pub mod opcode;
 pub mod signature;
+pub mod version;
 
+pub use self::classfile::ClassFile;
+pub use self::version::Version;
 pub use crate::classfile::attributes::Type;
 use crate::classfile::checker::{CheckResult, Checker};
 pub use crate::classfile::field_info::FieldInfo;
 pub use crate::classfile::method_info::MethodInfo;
 use crate::types::*;
-
-#[derive(Debug)]
-pub struct ClassFile {
-    pub version: Version,
-    pub cp: ConstantPool,
-    pub acc_flags: U2,
-    pub this_class: U2,
-    pub super_class: U2,
-    pub interfaces: Vec<U2>,
-    pub fields: Vec<FieldInfo>,
-    pub methods: Vec<MethodInfo>,
-    pub attrs: Vec<Type>,
-}
-
-impl ClassFile {
-    pub fn check_format(&self) -> CheckResult {
-        self.check(&self.cp)
-    }
-}
-
-#[derive(Debug)]
-pub struct Version {
-    pub minor: U2,
-    pub major: U2,
-}
-
-impl Checker for ClassFile {
-    fn check(&self, cp: &ConstantPool) -> CheckResult {
-        for it in self.cp.as_slice() {
-            let r = it.check(cp);
-            if r.is_err() {
-                return r;
-            }
-        }
-
-        Ok(())
-    }
-}
---
+++ src/classfile/version.rs
@@ -0,0 +1,7 @@
+use crate::types::U2;
+
+#[derive(Debug)]
+pub struct Version {
+    pub minor: U2,
+    pub major: U2,
+}
