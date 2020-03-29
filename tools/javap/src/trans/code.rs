1de035ebc0ffc3ccc63f60821c5dbd4ffbef9df0
Dou Chuan <1843657913@qq.com>
2020-03-29 15:15:50 +0800

    javap: impl CodeTranslator interp

--- tools/javap/src/trans/code.rs
+++ tools/javap/src/trans/code.rs
@@ -1,5 +1,5 @@
 use super::instruction::{get_instructions, InstructionInfo};
-use classfile::{attributes::Code, ClassFile};
+use classfile::{attributes::Code, ClassFile, OpCode};
 
 pub struct Translator<'a> {
     pub cf: &'a ClassFile,
@@ -7,8 +7,10 @@ pub struct Translator<'a> {
 }
 
 impl<'a> Translator<'a> {
-    pub fn get(&self) -> Vec<InstructionInfo> {
-        self.interp()
+    pub fn get(&self) -> Vec<String> {
+        let codes = self.code.code.as_slice();
+        let infos = self.interp();
+        infos.iter().map(|it| it.assemble(codes)).collect()
     }
 }
 
@@ -16,17 +18,33 @@ impl<'a> Translator<'a> {
     fn interp(&self) -> Vec<InstructionInfo> {
         let mut infos = vec![];
 
-        let instructions = get_instructions();
+        let mut instructions = get_instructions();
         let codes = self.code.code.as_slice();
         let codes_len = codes.len();
         let mut pc = 0;
+        let mut enable_wide = false;
         loop {
             if pc >= codes_len {
                 break;
             }
 
-            let instruction = instructions.get(codes[pc] as usize).unwrap();
+            let instruction = instructions.get_mut(codes[pc] as usize).unwrap();
+
+            if enable_wide {
+                instruction.set_wide(true);
+            }
+
             let (info, new_pc) = instruction.run(codes, pc);
+
+            if enable_wide {
+                instruction.set_wide(false);
+                enable_wide = false;
+            }
+
+            if info.op_code == OpCode::wide {
+                enable_wide = true;
+            }
+
             pc = new_pc;
             infos.push(info);
         }
