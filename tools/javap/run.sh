9c82c94b5f8a77e09a8b8ee9b969f6eb5e7fd1be
Dou Chuan <1843657913@qq.com>
2020-04-18 21:30:52 +0800

    javap: more cases

--- tools/javap/run.sh
+++ tools/javap/run.sh
@@ -4,25 +4,27 @@ export RUST_LOG=trace
 export RUST_BACKTRACE=full
 
 ### version
-#cargo run -- --version
+#cargo run -q -- --version
 
 ### line number
-#cargo run -- --cp test -l AbstractGraphicObject
-#cargo run -- --cp test -l GraphicObject
-#cargo run -- --cp test -l HelloWorld
-#cargo run -- --cp test -l EnumMobile
-#cargo run -- --cp test -l Interface1
-#cargo run -- --cp test -l Hockey
+#cargo run -q -- --cp test -l AbstractGraphicObject
+#cargo run -q -- --cp test -l GraphicObject
+#cargo run -q -- --cp test -l HelloWorld
+#cargo run -q -- --cp test -l EnumMobile
+#cargo run -q -- --cp test -l Interface1
+#cargo run -q -- --cp test -l Hockey
+#cargo run -q -- --cp test -l WifeAndMother
 
 ### disassemble
-#cargo run -- --cp test -c HelloWorld
+#cargo run -q -- --cp test -c HelloWorld
 
 ### disassemble & line number
-#cargo run -- --cp test -c -l HelloWorld
-#cargo run -- --cp test -c -l EnumMobile
+#cargo run -q -- --cp test -c -l HelloWorld
+#cargo run -q -- --cp test -c -l EnumMobile
 
 
-#cargo run -- --cp test --constants HelloWorld
+#cargo run -q -- --cp test --constants HelloWorld
+#cargo run -q -- --cp test --constants Hockey
 
 ### access flags
 #echo "default access flags"
@@ -42,3 +44,20 @@ export RUST_BACKTRACE=full
 #cargo run -q -- --cp test/testng-6.8.21.jar --sysinfo org.testng.collections.Lists
 #cargo run -q -- --cp test HelloWorld
 
+### Print internal type signatures
+#cargo run -q -- --cp test -s  HelloWorld
+### exception
+#cargo run -q -- --cp test  Ex
+#cargo run -q -- --cp test -s Ex
+#cargo run -q -- --cp test -c Ex
+#cargo run -q -- --cp test -v Ex
+
+#cargo run -q -- --cp test -v HelloWorld
+#cargo run -q -- --cp test/testng-6.8.21.jar -v org.testng.xml.XmlUtils
+#cargo run -q -- --cp test/testng-6.8.21.jar -v org.testng.xml.XmlUtils
+#cargo run -q -- --cp test/testng-6.8.21.jar -v org.testng.TestNG
+
+### contains generic params
+#cargo run -q -- --cp test/testng-6.8.21.jar  org.testng.TestNG
+#cargo run -q -- --cp test/testng-6.8.21.jar  -s org.testng.TestNG
+cargo run -q -- --cp test/testng-6.8.21.jar  -v org.testng.TestRunner
